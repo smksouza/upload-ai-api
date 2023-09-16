0894107f22a742070dd4cee857ec255861d6c669
Samuel Souza <smksouza.dev@gmail.com>
2023-09-16 15:06:54 -0300

    post mp3 e ultilização do pump

---
+++ src/routes/upload-video.ts
@@ -0,0 +1,35 @@
+import { FastifyInstance } from "fastify";
+import { fastifyMultipart } from "@fastify/multipart";
+import { prisma } from "../lib/prisma";
+import { randomUUID } from "node:crypto";
+import { pipeline } from "node:stream";
+import path from "node:path";
+import fs from "node:fs"
+
+export async function UploadVideoRoute(app: FastifyInstance) {
+    app.register(fastifyMultipart, {
+        limits: {
+            fileSize: 1_048_576 * 40,
+        }
+    })
+
+    app.post("/videos", async (request, reply) => {
+        const data = await request.file()
+
+        if (!data) {
+            return reply.status(400).send({ error: 'Missing file input.' })
+        }
+
+        const extension = path.extname(data.filename)
+
+        if (extension !== '.mp3') {
+            return reply.status(400).send({ error: 'Invalid input type, please upload a MP3.' })
+        }
+        const fileBaseName = path.basename(data.filename, extension)
+
+        const fileUploadName = `${fileBaseName}-${randomUUID()}${extension}`
+
+        const uploadDestination = path.resolve(__dirname, '../../tmp', fileUploadName)
+        await pump(data.file, fs.createWriteStream(data.filename))
+    })
+}

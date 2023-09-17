a1afbb358521c76e14b9bb7d5dd75759c606b5d1
Samuel Souza <smksouza.dev@gmail.com>
2023-09-17 11:33:33 -0300

    criação das routas da api

--- src/routes/get-all-prompts.ts
+++ src/routes/get-all-prompts.ts
@@ -1,7 +1,7 @@
 import { FastifyInstance } from "fastify";
 import { prisma } from "../lib/prisma";
 
-export async function getAllPromptsRout(app: FastifyInstance) {
+export async function getAllPromptsRoute(app: FastifyInstance) {
     app.get("/prompts", async () => {
         const prompts = await prisma.prompt.findMany()
         return prompts
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,15 @@
 import { fastify } from "fastify";
-import { prisma } from "./lib/prisma";
-import { getAllPromptsRout } from "./routes/get-all-prompts";
+import { getAllPromptsRoute } from "./routes/get-all-prompts";
 import { UploadVideoRoute } from "./routes/upload-video";
 import { createTranscriptionRoute } from "./routes/create-transcription";
+import { generateAICompletionRoute } from "./routes/generate-ai-completion"
 
 const app = fastify()
 
-app.register(getAllPromptsRout)
+app.register(getAllPromptsRoute)
 app.register(UploadVideoRoute)
 app.register(createTranscriptionRoute)
+app.register(generateAICompletionRoute)
 
 app.listen({
     port: 3333,
